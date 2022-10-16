166b129b551f315c6ee253c7ff2123c9887ea058
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2022-10-16 16:01:29 +0300

    new file

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || 'articles',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || '26092002artyr',
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
     }
 )
