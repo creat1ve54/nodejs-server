d98ab3df6e47e203c6b2641cf301aa6d5a1ba5f4
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2022-10-17 21:38:10 +0300

    first commit

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME || 'articles',
+    process.env.DB_NAME || 'top',
     process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || '26092002artyr',
+    process.env.DB_PASSWORD || '26092002Artyr@',
     {
         dialect: 'postgres',
         host: process.env.DB_HOST || 'localhost',
