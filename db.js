d673d5f5d99770319bde46962906bd2cc0995a34
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2022-10-22 11:47:41 +0300

    load

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD || '26092002artyr',
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || '127.0.0.1',
         port: process.env.DB_PORT || 5432,
     }
 )
